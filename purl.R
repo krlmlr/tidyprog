2446dcc12c41b86e50f986017d91c1d268c37dd9
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-06 19:28:58 +0200

    safe_purl()

--- purl.R
+++ purl.R
@@ -3,12 +3,25 @@ library(here)
 
 path <- "script/01-intro.Rmd"
 
+safe_copy <- function(source, target) {
+  if (!file.exists(target) || !identical(readLines(source), readLines(target))) {
+    file.copy(source, target, overwrite = TRUE)
+  }
+  invisible()
+}
+
+safe_purl <- function(text, output) {
+  tmp <- tempfile("purl", fileext = ".R")
+  knitr::purl(text = text, output = tmp, documentation = 0L)
+  safe_copy(tmp, output)
+}
+
 process_file <- function(path) {
   r_path <- here("proj", "script", gsub("Rmd$", "R", basename(path)))
 
   input <- readLines(here(path))
   header <- input[[6]]
-  knitr::purl(text = input[-(1:6)], output = r_path, documentation = 0L)
+  safe_purl(text = input[-(1:6)], output = r_path)
   r_path
 }
 
