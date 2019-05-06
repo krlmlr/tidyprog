ace239c8b213b3464245c2242f1c849fb4e345f0
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-06 19:20:11 +0200

    Split more

---
+++ script/35-args-multi.R
@@ -0,0 +1,45 @@
+### Multiple arguments
+
+library(tidyverse)
+library(here)
+
+
+weather_path <- function(filename) {
+  # Returned value
+  here("data/weather", filename)
+}
+read_weather_file <- function(filename) {
+  readxl::read_excel(weather_path(filename))
+}
+
+# Multiple function arguments are separated by comma:
+read_weather_data <- function(omit_zurich = FALSE, omit_toronto = FALSE) {
+  # Create ensemble dataset from files on disk
+  weather_data <- bind_rows(
+    berlin = read_weather_file("berlin.xlsx"),
+    toronto = read_weather_file("toronto.xlsx"),
+    tel_aviv = read_weather_file("tel_aviv.xlsx"),
+    zurich = read_weather_file("zurich.xlsx"),
+    .id = "city_code"
+  )
+
+  # Return it (filtered)
+  weather_data %>%
+    filter( !(city_code == "zurich" & omit_zurich) ) %>%
+    filter( !(city_code == "toronto" & omit_toronto) )
+}
+
+# Ellipsis: variable argument list, useful for passing arguments downstream
+weather_path <- function(...) {
+  # All arguments are passed on
+  here("data/weather", ...)
+}
+
+weather_path("berlin.xlsx")
+
+weather_path("some", "subdir", "with", "a", "file.csv")
+
+# Note: despite many changes and new features,
+# the original call still works!
+# (This can be tested automatically!)
+read_weather_data()
