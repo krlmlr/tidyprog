bfbb25c4b57c990dec7c6e92fb6a451f4229f8fd
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-08 23:26:56 +0200

    map-mutate

--- proj/script/10-mutate-map.R
+++ proj/script/10-mutate-map.R
@@ -3,33 +3,29 @@
 library(tidyverse)
 library(here)
 
-# Load dictionary from file
 dict <- readxl::read_excel(here("data/cities.xlsx"))
 dict
 
-# map() on a vector
 input_data <-
   dict %>%
   select(city_code, weather_filename) %>%
   deframe() %>%
   map(~ readxl::read_excel(here(.)))
 
-input_data
-
 # Lists are also vectors, enframe() returns a nested tibble!
 input_data %>%
   enframe()
 
-# Operate in "tibble-land": columns are vectors
+# Operate in "tibble-land" right away: columns are vectors
 dict %>%
   select(city_code, weather_filename) %>%
   mutate(
     data = map(weather_filename, ~ readxl::read_excel(here(.)))
   )
 
-# Keep auxiliary columns
+# Keep important columns
 dict %>%
   mutate(
     data = map(weather_filename, ~ readxl::read_excel(here(.)))
   ) %>%
-  select(city_code, name, data)
+  select(-weather_filename)
