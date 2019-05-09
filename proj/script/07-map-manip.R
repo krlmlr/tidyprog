d1ee28d4a2814092139bc80eb9a91ce66a02d93b
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-09 19:05:51 +0200

    Tweaks and exercises

--- proj/script/07-map-manip.R
+++ proj/script/07-map-manip.R
@@ -11,6 +11,10 @@ input_data <-
   deframe() %>%
   map(~ readxl::read_excel(here(.)))
 
+# Manipulate first result
+input_data[[1]] %>%
+  select(time, contains("emperature"))
+
 # Manipulate results
 input_data %>%
   map(~ select(., time, contains("emperature")))
@@ -39,3 +43,23 @@ manipulated_data
 
 # Shortcut
 map(input_data, manipulator)
+
+# Exercises
+
+input_data %>% 
+  map(~ summarize(., mean(temperature), mean(humidity)))
+
+compute_daily_mean <- function(data) {
+  data %>% 
+    group_by(as.Date(time)) %>% 
+    summarize(mean(temperature), mean(humidity)) %>% 
+    ungroup()
+}
+input_data %>% 
+  map(compute_daily_mean)
+
+input_data %>% 
+  map(dim)
+input_data %>% 
+  map(dim) %>% 
+  map(prod)
